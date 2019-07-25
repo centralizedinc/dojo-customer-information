da15e68a06d69424a703e0e622e79720802942c6
VPBelo <venus.belo.rhi@gmail.com>
2019-07-25 14:49:46 +0800

    update

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="register" id="components-form-demo-vuex">
-    <a-layout>
-      <a-layout-header class="test">DOJO SUPREMO “UNLEASHED YOUR GREATNESS”</a-layout-header>
-      <a-layout-content>
+  <a-layout id="customer-info">
+    <a-layout-header :style="{ width: '100%' }">
+      <div class="logo" />
+      <p
+        style="color: white; fontSize: font-size: 44px !important; text-align: center; font-style: oblique"
+      >DOJO SUPREMO “UNLEASHED YOUR GREATNESS”</p>
+    </a-layout-header>
+    <a-layout-content :style="{ padding: '0 50px', marginTop: ' 0px' }">
+      <br />
+      <p
+        style="fontSize: 24px;color: rgba(0, 0, 0, 0.85); marginBottom: 16px;fontWeight: 500"
+      >Customer Information</p>
+      <div :style="{ padding: '24px', minHeight: '380px' }">
         <a-card>
-          <a-form :form="form" @submit="handleSubmit">
+          <br />
+          <a-form :form="form">
             <a-form-item label="Firstname" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
               <a-input
                 placeholder="Please input your Firstname"
@@ -42,38 +52,38 @@
             </a-form-item>
             <a-form-item label="Membership" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
               <a-select v-model="customer.membership" placeholder="Please input your Lastname">
-                <a-select-option value="non_member">Non-Member</a-select-option>
-                <a-select-option value="member">Member</a-select-option>
-                <a-select-option value="walkin">Walkin</a-select-option>
+                <a-select-option value="Non-Member">Non-Member</a-select-option>
+                <a-select-option value="Member">Member</a-select-option>
+                <a-select-option value="Walkin">Walkin</a-select-option>
               </a-select>
             </a-form-item>
             <a-form-item label="Programmes" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
               <a-select v-model="customer.programmes" placeholder="Please select programmes">
-                <a-select-option value="dojo">DOJO</a-select-option>
-                <a-select-option value="boxing">BOXING</a-select-option>
-                <a-select-option value="aikido">IWAMA AIKIDO</a-select-option>
-                <a-select-option value="arnis">ARNIS</a-select-option>
-                <a-select-option value="luta_livre">BRAZILIAN LUTA LIVRE</a-select-option>
-                <a-select-option value="muay_thai">MUAY THAI</a-select-option>
-                <a-select-option value="martial_arts">MIXED MARTIAL ARTS</a-select-option>
-                <a-select-option value="taekwondo">TAEKWONDO</a-select-option>
-                <a-select-option value="aerobics">AEROBICS</a-select-option>
-                <a-select-option value="yoga">YOGA</a-select-option>c
-                <a-select-option value="zumba">ZUMBA</a-select-option>
+                <a-select-option value="DOJO">DOJO</a-select-option>
+                <a-select-option value="BOXING">BOXING</a-select-option>
+                <a-select-option value="IWAMA AIKIDO">IWAMA AIKIDO</a-select-option>
+                <a-select-option value="ARNIS">ARNIS</a-select-option>
+                <a-select-option value="BRAZILIAN LUTA LIVRE">BRAZILIAN LUTA LIVRE</a-select-option>
+                <a-select-option value="MUAY THAI">MUAY THAI</a-select-option>
+                <a-select-option value="MIXED MARTIAL ARTS">MIXED MARTIAL ARTS</a-select-option>
+                <a-select-option value="TAEKWONDO">TAEKWONDO</a-select-option>
+                <a-select-option value="AEROBICS">AEROBICS</a-select-option>
+                <a-select-option value="YOGA">YOGA</a-select-option>c
+                <a-select-option value="ZUMBA">ZUMBA</a-select-option>
               </a-select>
             </a-form-item>
             <a-form-item label="Gender" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
               <a-select v-model="customer.gender" placeholder="Please select gender">
-                <a-select-option value="male">Male</a-select-option>
-                <a-select-option value="female">Female</a-select-option>
+                <a-select-option value="Male">Male</a-select-option>
+                <a-select-option value="Female">Female</a-select-option>
               </a-select>
             </a-form-item>
             <a-form-item label="Status" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
               <a-select v-model="customer.status" placeholder="Please select status">
-                <a-select-option value="single">Single</a-select-option>
-                <a-select-option value="married">Married</a-select-option>
-                <a-select-option value="divorced">Divorced</a-select-option>
-                <a-select-option value="widowed">Widowed</a-select-option>
+                <a-select-option value="Single">Single</a-select-option>
+                <a-select-option value="Married">Married</a-select-option>
+                <a-select-option value="Divorced">Divorced</a-select-option>
+                <a-select-option value="Widowed">Widowed</a-select-option>
               </a-select>
             </a-form-item>
             <a-form-item
@@ -94,12 +104,11 @@
             </a-col>
           </a-row>
         </a-card>
-      </a-layout-content>
-      <a-layout-footer></a-layout-footer>
-    </a-layout>
-  </div>
+      </div>
+    </a-layout-content>
+    <a-layout-footer :style="{ textAlign: 'center' }">DOJO SUPREMO ©2018 Created by DOJO TEAM</a-layout-footer>
+  </a-layout>
 </template>
-
 <script>
 import axios from "axios";
 // import dojo from "../assets/dojo.png"
@@ -107,6 +116,8 @@ export default {
   data() {
     return {
       // dojo: dojo,
+      formLayout: "horizontal",
+      form: this.$form.createForm(this),
       customer: {
         name: {
           first_name: "",
@@ -156,17 +167,6 @@ export default {
       }
     };
   },
-  // computed: {
-  //   username() {
-  //     return this.$store.state.username;
-  //   }
-  // },
-  // watch: {
-  // //   username(val) {
-  // //     console.log("this.$store.state.username: ", val);
-  // //     this.form.setFieldsValue({ username: val });
-  // //   }
-  // // },
   watch: {
     customer(val) {
       console.log("val :", val);
@@ -192,21 +192,6 @@ export default {
     });
   },
   methods: {
-    handleSelectChange(value) {
-      console.log(value);
-      this.form.setFieldsValue({
-        note: `Hi, ${value === "male" ? "man" : "lady"}!`
-      });
-    },
-    handleSubmit(e) {
-      e.preventDefault();
-      this.form.validateFields((err, values) => {
-        if (!err) {
-          console.log("Received values of form: ", values);
-          this.$store.commit("update", values);
-        }
-      });
-    },
     handleSubmit(e) {
       e.preventDefault();
       this.form.validateFields((err, fieldsValue) => {
@@ -265,25 +250,12 @@ export default {
 };
 </script>
 <style>
-#components-form-demo-vuex .language-bash {
-  max-width: 400px;
-  border-radius: 6px;
-  margin-top: 24px;
-}
-#components-form-demo-vuex .ant-layout-content {
-  background: rgb(235, 204, 156);
-  color: rgb(243, 81, 94);
-  min-height: 100px;
-  height: 400px !important;
-}
-#components-form-demo-vuex .ant-layout-footer {
-  line-height: 60;
-  background: rgba(34, 8, 0, 0.2);
-}
-.test {
-  color: white !important;
-  font-size: 40px !important;
-  text-align: center !important;
-  font-style: oblique !important;
+#customer-info .logo {
+  width: 120px;
+  height: 31px;
+  background: rgb(0, 21, 41);
+  margin: 16px 24px 16px 0;
+  float: left;
 }
 </style>
+
