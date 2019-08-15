456488b0663472aabdb10c226f084e5a1d034fbb
VPBelo <venus.belo.rhi@gmail.com>
2019-08-15 09:10:52 +0800

    update

--- src/views/View.vue
+++ src/views/View.vue
@@ -1,37 +1,107 @@
 <template>
-  <a-card title="Customer Details">
-    <p>{{customer.name}}</p>
-    <p>card content</p>
-    <p>card content</p>
-    <a-row>
-      <a-col :span="2" :style="{ textAlign: 'right' }">
-        <a-button :style="{ marginLeft: '8px' }" @click="Cancel">Back</a-button>
-      </a-col>
-    </a-row>
-  </a-card>
+  <a-table :columns="columns" :dataSource="data">
+    <a slot="name" slot-scope="text" href="javascript:;">{{text}}</a>
+    <span slot="customTitle">
+      <a-icon type="smile-o" />Name
+    </span>
+    <span slot="tags" slot-scope="tags">
+      <a-tag v-for="tag in tags" color="blue" :key="tag">{{tag}}</a-tag>
+    </span>
+    <span slot="action">
+      <a href="javascript:;">Time in</a>
+      <a-divider type="vertical" />
+      <a href="javascript:;">View</a>
+    </span>
+  </a-table>
 </template>
-
 <script>
+const columns = [
+  {
+    title: "Fullname",
+    dataIndex: "name",
+    key: "name",
+    // slots: { title: 'customTitle' },
+    scopedSlots: { customRender: "name" }
+  },
+  {
+    title: "Programmes",
+    key: "tags",
+    dataIndex: "tags",
+    scopedSlots: { customRender: "tags" }
+  },
+  {
+    title: "Package Type",
+    dataIndex: "age",
+    key: "age"
+  },
+  {
+    title: "Membership",
+    dataIndex: "address",
+    key: "address"
+  },
+  {
+    title: "Valid until",
+    dataIndex: "address",
+    key: "address"
+  },
+  {
+    title: "Session",
+    children: [
+      {
+        title: "Used Session",
+        dataIndex: "companyAddress",
+        key: "companyAddress"
+      },
+      {
+        title: "Total Session",
+        dataIndex: "companyName",
+        key: "companyName"
+      }
+    ]
+  },
+  {
+    title: "Action",
+    key: "action",
+    scopedSlots: { customRender: "action" }
+  }
+];
+
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+    companyAddress: 'Lake Street 42',
+    companyName: 'SoftLake Co'
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    companyAddress: 'Lake Street 42',
+    companyName: 'SoftLake Co',
+    tags: ["loser"]
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    companyAddress: 'Lake Street 42',
+    companyName: 'SoftLake Co',
+    tags: ["cool", "teacher"]
+  }
+];
+
 export default {
   data() {
     return {
-      customer: {}
+      data,
+      columns
     };
-  },
-
-  created() {
-    this.init();
-  },
-  methods: {
-    init() {
-      this.customer = this.$store.state.selectedCustomer;
-    },
-    Cancel() {
-      this.$router.push("/");
-    }
   }
 };
 </script>
-
-<style>
-</style>
